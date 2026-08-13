b18f609889643f5cf3f2e4e667d5bb1c36017315
Shweta9975 <shwetamane9696@gmail.com>
2026-08-13 20:40:17 +0530

    Add Module1 and Module2 projects

---
+++ Module2/springbootwebapplication/src/test/java/com/module2/springbootwebapplication/springbootwebapplication/SpringbootwebapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module2.springbootwebapplication.springbootwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
