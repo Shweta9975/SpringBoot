731873231db1751d68af8c3d7516727f58c66be4
Shweta9975 <shwetamane9696@gmail.com>
2026-08-18 12:05:40 +0530

    Used @RequestParam inside the EmployeeController

--- Module2/springbootwebapplication/src/main/java/com/module2/springbootwebapplication/springbootwebapplication/Controller/EmployeeController.java
+++ Module2/springbootwebapplication/src/main/java/com/module2/springbootwebapplication/springbootwebapplication/Controller/EmployeeController.java
@@ -2,9 +2,11 @@ package com.module2.springbootwebapplication.springbootwebapplication.Controller
 
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.module2.springbootwebapplication.springbootwebapplication.DTO.EmployeeDTO;
@@ -20,6 +22,13 @@ public class EmployeeController {
 	@GetMapping("/employees/{employeeId}")
 	public EmployeeDTO getEmployeeById(@PathVariable Long employeeId) {
 		return new EmployeeDTO(employeeId,"Shweta","shwetamane@gmail.com",25,LocalDate.of(2026, 8, 18),true);
+		
+	}
+	
+	@GetMapping(path="/employees")
+	public String getAllEmployees(@RequestParam(required=false) Integer age,
+			                      @RequestParam(required=false) String sortBy){ //used required to make parameter optional
+		return "Hi age "+age+ " "+sortBy;
 		
 	}
 }
