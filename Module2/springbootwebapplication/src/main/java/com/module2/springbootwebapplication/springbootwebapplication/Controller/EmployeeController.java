9f2a030557fd3478b9ee4664af7a0c90f110f7eb
Shweta9975 <shwetamane9696@gmail.com>
2026-08-18 12:11:08 +0530

    Used @RequestMapping inside the EmployeeController

--- Module2/springbootwebapplication/src/main/java/com/module2/springbootwebapplication/springbootwebapplication/Controller/EmployeeController.java
+++ Module2/springbootwebapplication/src/main/java/com/module2/springbootwebapplication/springbootwebapplication/Controller/EmployeeController.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.module2.springbootwebapplication.springbootwebapplication.DTO.EmployeeDTO;
 
 @RestController
+@RequestMapping(path="/employees")
 public class EmployeeController {
 
 //	@GetMapping(path="/getSecretMessage")
@@ -19,13 +21,21 @@ public class EmployeeController {
 //		return "asdgh@#$DASD";
 //}
 	
-	@GetMapping("/employees/{employeeId}")
+	@GetMapping("/{employeeId}")
 	public EmployeeDTO getEmployeeById(@PathVariable Long employeeId) {
 		return new EmployeeDTO(employeeId,"Shweta","shwetamane@gmail.com",25,LocalDate.of(2026, 8, 18),true);
 		
 	}
 	
-	@GetMapping(path="/employees")
+//	@GetMapping(path="/employees")
+//	public String getAllEmployees(@RequestParam(required=false) Integer age,
+//			                      @RequestParam(required=false) String sortBy){ //used required to make parameter optional
+//		return "Hi age "+age+ " "+sortBy;
+//		
+//	}
+	
+	//we can use GetMapping without ("/employees") since we are inside employeecontroller
+	@GetMapping
 	public String getAllEmployees(@RequestParam(required=false) Integer age,
 			                      @RequestParam(required=false) String sortBy){ //used required to make parameter optional
 		return "Hi age "+age+ " "+sortBy;
