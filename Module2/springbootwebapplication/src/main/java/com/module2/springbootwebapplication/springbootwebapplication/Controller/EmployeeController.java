8efc4901af9f9104c037d2b6ba1bda60f9336c81
Shweta9975 <shwetamane9696@gmail.com>
2026-08-18 11:51:21 +0530

    Used @PathVariavle and @GetMapping inside EmployeeController to get the Employee

--- Module2/springbootwebapplication/src/main/java/com/module2/springbootwebapplication/springbootwebapplication/Controller/EmployeeController.java
+++ Module2/springbootwebapplication/src/main/java/com/module2/springbootwebapplication/springbootwebapplication/Controller/EmployeeController.java
@@ -1,15 +1,25 @@
 package com.module2.springbootwebapplication.springbootwebapplication.Controller;
 
 
+import java.time.LocalDate;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.module2.springbootwebapplication.springbootwebapplication.DTO.EmployeeDTO;
 
 @RestController
 public class EmployeeController {
 
-	@GetMapping(path="/getSecretMessage")
-	public String getMySuperSecretMessage() {
-		return "asdgh@#$DASD";
-}
+//	@GetMapping(path="/getSecretMessage")
+//	public String getMySuperSecretMessage() {
+//		return "asdgh@#$DASD";
+//}
 	
+	@GetMapping("/employees/{employeeId}")
+	public EmployeeDTO getEmployeeById(@PathVariable Long employeeId) {
+		return new EmployeeDTO(employeeId,"Shweta","shwetamane@gmail.com",25,LocalDate.of(2026, 8, 18),true);
+		
+	}
 }
--- Module2/springbootwebapplication/src/main/java/com/module2/springbootwebapplication/springbootwebapplication/DTO/EmployeeDTO.java
+++ Module2/springbootwebapplication/src/main/java/com/module2/springbootwebapplication/springbootwebapplication/DTO/EmployeeDTO.java
@@ -7,6 +7,7 @@ public class EmployeeDTO {
 	private Long id;
 	private String name;
 	private String email;
+	private Integer age;
 	private LocalDate dateOfJoining;
 	private Boolean isActive;
 	
@@ -18,14 +19,21 @@ public class EmployeeDTO {
 	}
 	
 	
-	public EmployeeDTO(Long id, String name, String email, LocalDate dateOfJoining, Boolean isActive) {
+	
+
+
+	public EmployeeDTO(Long id, String name, String email, Integer age, LocalDate dateOfJoining, Boolean isActive) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
+		this.age = age;
 		this.dateOfJoining = dateOfJoining;
 		this.isActive = isActive;
 	}
+
+
+
 
 
 	public Long getId() {
@@ -57,6 +65,16 @@ public class EmployeeDTO {
 	}
 	public void setIsActive(Boolean isActive) {
 		this.isActive = isActive;
+	}
+
+
+	public Integer getAge() {
+		return age;
+	}
+
+
+	public void setAge(Integer age) {
+		this.age = age;
 	}
 	
 	
